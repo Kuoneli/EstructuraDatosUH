95c98125bff4d3bc4f8f55a7c0693b8abd628a0c
Kuoneli <stevcv@hotmail.com>
2022-09-30 23:01:49 -0600

    Trabajo de Clase

---
+++ UHClase3/Arrays, Vectores y mas/src/App.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        int i,j,k;
+        int [][][] datos;
+        datos = new int[3][5][12];
+
+
+
+
+
+        }
+    }
